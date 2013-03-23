c4a2ca9e1d93cabf854fe14b3b08a90bbb3a9034
oscaroviedo13 <oscaroviedo13@gmail.com>
2013-03-22 22:44:16 -0500

    Cambio nombre forma principal

--- plantilla/general.php
+++ plantilla/general.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Internet Dreams</title>
+<title>Punto de Venta.</title>
 <link rel="stylesheet" href="../css/screen.css" type="text/css" media="screen" title="default" />
 <!--[if IE]>
 <link rel="stylesheet" media="all" type="text/css" href="css/pro_dropline_ie.css" />
