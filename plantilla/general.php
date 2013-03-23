975fa3b5512666b107ab8dc2651987c1c7758d5f
oscaroviedo13 <oscaroviedo13@gmail.com>
2013-03-22 22:48:07 -0500

    General

--- plantilla/general.php
+++ plantilla/general.php
@@ -260,12 +260,12 @@ session_start();
 		<div class="nav">
 		<div class="table">
 		
-		<ul class="select"><li><a href="#nogo"><b>Dashboard ñ</b><!--[if IE 7]><!--></a><!--<![endif]-->
+		<ul class="select"><li><a href="#nogo"><b>Productos</b><!--[if IE 7]><!--></a><!--<![endif]-->
 		<!--[if lte IE 6]><table><tr><td><![endif]-->
 		<div class="select_sub">
 			<ul class="sub">
                             
-                                <li><a href="../productos/buscar2.php" target="CONTENIDO" >Dashboard Details 1</a></li>
+                                <li><a href="../productos/buscar2.php" target="CONTENIDO" >Listado de productos</a></li>
 				<li><a href="#nogo">Dashboard Details 2</a></li>
 				<li><a href="#nogo">Dashboard Details 3</a></li>
 			</ul>
