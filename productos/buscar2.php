5a974e1ad10d191acde5719e4cc154e70d161b8b
oscaroviedo13 <oscaroviedo13@gmail.com>
2013-03-23 10:30:23 -0500

    Cargar paginacion
    Se crear la paginacion en la pagina que maneja los productos del
    inventario.

--- productos/buscar2.php
+++ productos/buscar2.php
@@ -1,174 +1,175 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Internet Dreams</title>
-<link rel="stylesheet" href="../css/screen.css" type="text/css" media="screen" title="default" />
-<!--[if IE]>
-<link rel="stylesheet" media="all" type="text/css" href="css/pro_dropline_ie.css" />
-<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Internet Dreams</title>
+    <link rel="stylesheet" href="../css/screen.css" type="text/css" media="screen" title="default" />
+    <link rel="stylesheet" href="../css/estilo.css" type="text/css" media="screen" title="default" />
+    <!--[if IE]>
+    <link rel="stylesheet" media="all" type="text/css" href="css/pro_dropline_ie.css" />
+    <![endif]-->
 
-<!--  jquery core -->
-<script src="../js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
+    <!--  jquery core -->
+    <script src="../js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
 
-<!--  checkbox styling script -->
-<script src="../js/jquery/ui.core.js" type="text/javascript"></script>
-<script src="../js/jquery/ui.checkbox.js" type="text/javascript"></script>
-<script src="../js/jquery/jquery.bind.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(function(){
-	$('input').checkBox();
-	$('#toggle-all').click(function(){
- 	$('#toggle-all').toggleClass('toggle-checked');
-	$('#mainform input[type=checkbox]').checkBox('toggle');
-	return false;
-	});
-});
-</script>  
+    <!--  checkbox styling script -->
+    <script src="../js/jquery/ui.core.js" type="text/javascript"></script>
+    <script src="../js/jquery/ui.checkbox.js" type="text/javascript"></script>
+    <script src="../js/jquery/jquery.bind.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(function(){
+            $('input').checkBox();
+            $('#toggle-all').click(function(){
+            $('#toggle-all').toggleClass('toggle-checked');
+            $('#mainform input[type=checkbox]').checkBox('toggle');
+            return false;
+            });
+    });
+    </script>  
 
-<![if !IE 7]>
+    <![if !IE 7]>
 
-<!--  styled select box script version 1 -->
-<script src="js/jquery/jquery.selectbox-0.5.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-	$('.styledselect').selectbox({ inputClass: "selectbox_styled" });
-});
-</script>
- 
+    <!--  styled select box script version 1 -->
+    <script src="js/jquery/jquery.selectbox-0.5.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+            $('.styledselect').selectbox({ inputClass: "selectbox_styled" });
+    });
+    </script>
 
-<![endif]>
 
-<!--  styled select box script version 2 --> 
-<script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-	$('.styledselect_form_1').selectbox({ inputClass: "styledselect_form_1" });
-	$('.styledselect_form_2').selectbox({ inputClass: "styledselect_form_2" });
-});
-</script>
+    <![endif]>
 
-<!--  styled select box script version 3 --> 
-<script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-	$('.styledselect_pages').selectbox({ inputClass: "styledselect_pages" });
-});
-</script>
+    <!--  styled select box script version 2 --> 
+    <script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+            $('.styledselect_form_1').selectbox({ inputClass: "styledselect_form_1" });
+            $('.styledselect_form_2').selectbox({ inputClass: "styledselect_form_2" });
+    });
+    </script>
 
-<!--  styled file upload script --> 
-<script src="js/jquery/jquery.filestyle.js" type="text/javascript"></script>
-<script type="text/javascript" charset="utf-8">
-  $(function() {
-      $("input.file_1").filestyle({ 
-          image: "images/forms/choose-file.gif",
-          imageheight : 21,
-          imagewidth : 78,
-          width : 310
+    <!--  styled select box script version 3 --> 
+    <script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+            $('.styledselect_pages').selectbox({ inputClass: "styledselect_pages" });
+    });
+    </script>
+
+    <!--  styled file upload script --> 
+    <script src="js/jquery/jquery.filestyle.js" type="text/javascript"></script>
+    <script type="text/javascript" charset="utf-8">
+      $(function() {
+          $("input.file_1").filestyle({ 
+              image: "images/forms/choose-file.gif",
+              imageheight : 21,
+              imagewidth : 78,
+              width : 310
+          });
       });
-  });
-</script>
+    </script>
 
-<!-- Custom jquery scripts -->
-<script src="js/jquery/custom_jquery.js" type="text/javascript"></script>
- 
-<!-- Tooltips -->
-<script src="../js/jquery/jquery.tooltip.js" type="text/javascript"></script>
-<script src="js/jquery/jquery.dimensions.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(function() {
-	$('a.info-tooltip ').tooltip({
-		track: true,
-		delay: 0,
-		fixPNG: true, 
-		showURL: false,
-		showBody: " - ",
-		top: -35,
-		left: 5
-	});
-});
-</script> 
+    <!-- Custom jquery scripts -->
+    <script src="js/jquery/custom_jquery.js" type="text/javascript"></script>
+
+    <!-- Tooltips -->
+    <script src="../js/jquery/jquery.tooltip.js" type="text/javascript"></script>
+    <script src="js/jquery/jquery.dimensions.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(function() {
+            $('a.info-tooltip ').tooltip({
+                    track: true,
+                    delay: 0,
+                    fixPNG: true, 
+                    showURL: false,
+                    showBody: " - ",
+                    top: -35,
+                    left: 5
+            });
+    });
+    </script> 
 
 
-<!--  date picker script -->
-<link rel="stylesheet" href="css/datePicker.css" type="text/css" />
-<script src="js/jquery/date.js" type="text/javascript"></script>
-<script src="js/jquery/jquery.datePicker.js" type="text/javascript"></script>
-<script type="text/javascript" charset="utf-8">
-        $(function()
-{
+    <!--  date picker script -->
+    <link rel="stylesheet" href="css/datePicker.css" type="text/css" />
+    <script src="js/jquery/date.js" type="text/javascript"></script>
+    <script src="js/jquery/jquery.datePicker.js" type="text/javascript"></script>
+    <script type="text/javascript" charset="utf-8">
+            $(function()
+    {
 
-// initialise the "Select date" link
-$('#date-pick')
-	.datePicker(
-		// associate the link with a date picker
-		{
-			createButton:false,
-			startDate:'01/01/2005',
-			endDate:'31/12/2020'
-		}
-	).bind(
-		// when the link is clicked display the date picker
-		'click',
-		function()
-		{
-			updateSelects($(this).dpGetSelected()[0]);
-			$(this).dpDisplay();
-			return false;
-		}
-	).bind(
-		// when a date is selected update the SELECTs
-		'dateSelected',
-		function(e, selectedDate, $td, state)
-		{
-			updateSelects(selectedDate);
-		}
-	).bind(
-		'dpClosed',
-		function(e, selected)
-		{
-			updateSelects(selected[0]);
-		}
-	);
-	
-var updateSelects = function (selectedDate)
-{
-	var selectedDate = new Date(selectedDate);
-	$('#d option[value=' + selectedDate.getDate() + ']').attr('selected', 'selected');
-	$('#m option[value=' + (selectedDate.getMonth()+1) + ']').attr('selected', 'selected');
-	$('#y option[value=' + (selectedDate.getFullYear()) + ']').attr('selected', 'selected');
-}
-// listen for when the selects are changed and update the picker
-$('#d, #m, #y')
-	.bind(
-		'change',
-		function()
-		{
-			var d = new Date(
-						$('#y').val(),
-						$('#m').val()-1,
-						$('#d').val()
-					);
-			$('#date-pick').dpSetSelected(d.asString());
-		}
-	);
+    // initialise the "Select date" link
+    $('#date-pick')
+            .datePicker(
+                    // associate the link with a date picker
+                    {
+                            createButton:false,
+                            startDate:'01/01/2005',
+                            endDate:'31/12/2020'
+                    }
+            ).bind(
+                    // when the link is clicked display the date picker
+                    'click',
+                    function()
+                    {
+                            updateSelects($(this).dpGetSelected()[0]);
+                            $(this).dpDisplay();
+                            return false;
+                    }
+            ).bind(
+                    // when a date is selected update the SELECTs
+                    'dateSelected',
+                    function(e, selectedDate, $td, state)
+                    {
+                            updateSelects(selectedDate);
+                    }
+            ).bind(
+                    'dpClosed',
+                    function(e, selected)
+                    {
+                            updateSelects(selected[0]);
+                    }
+            );
 
-// default the position of the selects to today
-var today = new Date();
-updateSelects(today.getTime());
+    var updateSelects = function (selectedDate)
+    {
+            var selectedDate = new Date(selectedDate);
+            $('#d option[value=' + selectedDate.getDate() + ']').attr('selected', 'selected');
+            $('#m option[value=' + (selectedDate.getMonth()+1) + ']').attr('selected', 'selected');
+            $('#y option[value=' + (selectedDate.getFullYear()) + ']').attr('selected', 'selected');
+    }
+    // listen for when the selects are changed and update the picker
+    $('#d, #m, #y')
+            .bind(
+                    'change',
+                    function()
+                    {
+                            var d = new Date(
+                                                    $('#y').val(),
+                                                    $('#m').val()-1,
+                                                    $('#d').val()
+                                            );
+                            $('#date-pick').dpSetSelected(d.asString());
+                    }
+            );
 
-// and update the datePicker to reflect it...
-$('#d').trigger('change');
-});
-</script>
+    // default the position of the selects to today
+    var today = new Date();
+    updateSelects(today.getTime());
 
-<!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
-<script src="js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-$(document).pngFix( );
-});
-</script>
+    // and update the datePicker to reflect it...
+    $('#d').trigger('change');
+    });
+    </script>
+
+    <!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
+    <script src="js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+    $(document).pngFix( );
+    });
+    </script>
 </head>
     
   
@@ -211,10 +212,32 @@ $(document).pngFix( );
 					<th class="table-header-options line-left"><a href="">Options</a></th>
 				</tr>
                                 <?php 
-                                    $consulta=mysql_query("select * from productos");
+                                    $consulta=mysql_query("select * from productos order by nombre");
                                     if (isset($_POST['buscar'])){
-                                            $consulta=mysql_query("select * from productos where nombre like '%".$_POST['buscar']."%'");
+                                            $consulta=mysql_query("select1 * from productos where nombre like '%".$_POST['buscar']."%'");
                                     }
+                                        
+                                    $nro_reg=mysql_num_rows($consulta);
+
+                                if ($nro_reg==0){
+                                        echo 'no se han encontrado productos para mostrar';
+                                }
+
+                                $reg_por_pagina=10;
+                                if (isset($_GET['num'])){
+                                        $nro_pagina=$_GET['num'];	
+                                }else{
+                                        $nro_pagina=1;
+                                }
+                                
+                                if (is_numeric($nro_pagina))
+                                        $inicio=(($nro_pagina-1)*$reg_por_pagina);
+
+                                else
+                                        $inicio=0;
+
+                                        $consulta=mysql_query("select * from productos order by nombre limit  $inicio,$reg_por_pagina",$conexion);
+                                        $can_paginas=ceil($nro_reg/$reg_por_pagina);
                                         
                                         while($filas=mysql_fetch_array($consulta)){
                                                 $id=$filas['id'];
@@ -257,31 +280,25 @@ $(document).pngFix( );
 			<table border="0" cellpadding="0" cellspacing="0" id="paging-table">
 			<tr>
 			<td>
-<!--				Aqui va el paginador.-->
-                            <?php
-                                $nro_reg=mysql_num_rows($consulta);
 
-                                if ($nro_reg==0){
-                                        echo 'no se han encontrado productos para mostrar';
-                                }
+                            <div id ="paginador" align="center">
+                            <?php 
 
-                                $reg_por_pagina=4;
-                                if (isset($_GET['num'])){
-                                        $nro_pagina=$_GET['num'];	
-                                }else{
-                                        $nro_pagina=1;
-                                }
-                                
-                                if (is_numeric($nro_pagina))
-                                        $inicio=(($nro_pagina-1)*$reg_por_pagina);
+                                    if($nro_pagina>1)
+                                            echo "<a href='buscar2.php?num=".($nro_pagina-1)."'>Anterior</a> ";
 
-                                else
-                                        $inicio=0;
+                                    for ($i=1;$i<=$can_paginas;$i++){
+                                            if ($i==$nro_pagina)
+                                                    echo "<span>$i </span> ";
+                                                    //echo $i."  ";
+                                            else
+                                            echo "<a href='buscar2.php?num=$i'>$i</a> ";
 
-                                        $consulta=mysql_query("select * from productos order by nombre limit  $inicio,$reg_por_pagina",$conexion);
-                                        $can_paginas=ceil($nro_reg/$reg_por_pagina);
-                                
+                                    }
+                                    if($nro_pagina<$can_paginas)
+                                            echo "<a href='buscar2.php?num=".($nro_pagina+1)."'>Siguiente</a> ";
                             ?>
+                            </div>
 			</td>			
 			</tr>
 			</table>
